f3e7c479a9982b9deaa35b2a26a06312d9647868
Theo Fornetti <Theofornetti2001@gmail.com>
2024-09-01 20:21:23 -0300

    arreglo

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-// server.js
 const express = require("express");
-const path = require("path");
-const app = express();
-const port = process.env.port || 3001;
-const wsPort = process.env.port || 8080;
-const twilio = import("twilio");
+const bodyParser = require("body-parser");
 const WebSocket = require("ws");
 const http = require("http");
-const server = http.createServer();
 
-// Crear un servidor WebSocket
+const app = express();
+const port = process.env.PORT || 3001; // Usa la variable de entorno PORT para compatibilidad con servicios de despliegue
+
+// Crear un servidor HTTP que maneje tanto Express como WebSocket
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-
-const bodyParser = require("body-parser");
 
 // Middleware para analizar los datos del cuerpo de la solicitud
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,38 +19,25 @@ app.get("/", (req, res) => {
 
 app.post("/messageIn", (req, res) => {
   const fromNumber = req.body.From;
-  console.log("Mensaje Recivido")
+  console.log("Mensaje Recibido");
   console.log(fromNumber);
-
-  //broadcast("Hola Theo");
   res.sendStatus(200);
 });
 
-//Como vamos a hacerf para identificar los diferentes ingresos
-//el qr deberia tener algo para hacerlo asi sabermos cual barrear devemos levantar
-//como vamos a hacer para evitar que envien mensajes falopa
-// para que no levanten la barrea seguido (Segunda opcion formulario en el servidor con autenticacion de google etc)
-
-// Inicia el servidor
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}/`);
-});
-
 wss.on("connection", (ws) => {
-  console.log("Cliente conectado");
+  console.log("Cliente WebSocket conectado");
 
   ws.on("message", (message) => {
     console.log(`Mensaje recibido: ${message}`);
-    // Enviar el mensaje recibido de vuelta al cliente
     ws.send(`Recibido: ${message}`);
   });
 
   ws.on("close", () => {
-    console.log("Cliente desconectado");
+    console.log("Cliente WebSocket desconectado");
   });
 });
 
-// Iniciar el servidor HTTP en el puerto 8080
-server.listen(wsPort, () => {
-  console.log("Servidor WebSocket escuchando en ws://localhost:",wsPort);
+// Inicia el servidor en el puerto especificado por la variable de entorno PORT
+server.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}/`);
 });
